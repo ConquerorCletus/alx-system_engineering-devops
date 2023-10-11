d3ef1c083244bde08836d26c2f746bd5a3ff9397
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-11 03:52:37 +0100

    top ten with reddit api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""This moduke contains a function that
-queries the Reddit API and prints the titles
+"""This module contains
+a function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit."""
+import requests
 
 
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     try:
