4571b72ff3eb7725495a2ae0d472847580617a36
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-19 18:02:03 +0100

    sky not the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-# A puppet script to increase
-# the amount of traffic an Nginx server can handle.
-# Increase the ULIMIT of the default file
+# Increases the amount of traffic an Nginx server can handle.
 
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
