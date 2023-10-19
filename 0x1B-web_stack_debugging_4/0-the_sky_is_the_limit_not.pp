422a900c82b2d39005160adf21e66db73d64ad24
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-19 18:47:09 +0100

    new update Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,10 @@
-# Increase the ULIMIT for Nginx
-exec { 'fix-for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin'],
-  onlyif  => "grep -q '4096' /etc/default/nginx",
-  require => File['/etc/default/nginx'],
+# Increase the ULIMIT for Nginx to fix bug
+exec { 'fix--for-nginx':
+  command => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Define a file resource to manage the Nginx config
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => template('nginx/default.nginx.erb'), # Use a Puppet template or provide the content here.
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-# Restart Nginx service when the config changes
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  subscribe => Exec['fix-for-nginx'],
-}
-
